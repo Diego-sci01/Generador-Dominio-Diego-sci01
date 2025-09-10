7e5a5a69458b770d7d8d680d9d3d20b763fbc4f5
Diego-sci01 <diegosantosantana2008@gmail.com>
2025-09-10 19:02:38 +0000

    Listo

---
+++ app.js
@@ -0,0 +1,7 @@
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  console.log (pronoun[Math.floor(Math.random() * pronoun.length)]);
+  console.log (adj[Math.floor(Math.random() * pronoun.length)]);
+  console.log (noun[Math.floor(Math.random() * pronoun.length)]);
